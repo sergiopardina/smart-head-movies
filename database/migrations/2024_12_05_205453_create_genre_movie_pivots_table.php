cb54c02b1b4518e33a1351b26d3737e53131a878
Serkhio Pardina <s.pardina@jetdents.com>
2024-12-05 22:34:04 +0100

    Make genre_movie_pivots fields nullable

--- database/migrations/2024_12_05_205453_create_genre_movie_pivots_table.php
+++ database/migrations/2024_12_05_205453_create_genre_movie_pivots_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('genre_movie_pivots', function (Blueprint $table) {
             $table->id();
-            $table->integer('genre_id');
-            $table->integer('movie_id');
+            $table->integer('genre_id')->nullable();
+            $table->integer('movie_id')->nullable();
 
             $table->timestamps();
 
